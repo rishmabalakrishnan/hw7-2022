cbefd4e2cb73a3a02ad42ad48d1e7e94bf7e21aa
rishmabalakrishnan <72809155+rishmabalakrishnan@users.noreply.github.com>
2022-11-05 17:57:52 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -17,3 +17,42 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.9;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime += 10 <= video.duration) {
+		video.currentTime += 10;
+	}
+	else {
+		video.currentTime = 0;
+	}
+	console.log("Current location is " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {
+		video.muted = true;
+		this.innerHTML = "Unmute";
+	}
+	else {
+		video.muted = false;
+		this.innerHTML = "Mute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log("inside volume fxn");
+	video.volume = this.value / 100;
+	volDisplay = document.querySelector("#volume");
+	volDisplay.innerHTML = (video.volume * 100) + '%';
+});
+
+// document.querySelector("#")
